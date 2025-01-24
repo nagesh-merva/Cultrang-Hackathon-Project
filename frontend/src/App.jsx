1de35f26fbe30c0187b4b5f31eb9bd36aaa31c17
nagesh-merva <officialnagesh.merva@gmail.com>
2025-01-24 22:54:44 +0530

    Setting up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <p className="text-black font-bold text-6xl place-self-center">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
