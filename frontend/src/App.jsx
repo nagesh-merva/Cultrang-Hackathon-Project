65b1b34d83c6df6af7e9ba4666590001136e4c7f
nagesh-merva <officialnagesh.merva@gmail.com>
2025-01-25 15:41:40 +0530

    Integrating Phase 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/OtherPages/LandingPage";
+import Auth from "./pages/Recruiter/Auth";
+import ProfilePage from "./pages/Recruiter/CompanyProfile";
+import RecruiterPage from "./pages/RecruiterPage/RecruiterPage";
 
 const App = () => {
-  return <div></div>;
-};
+  return <div>
+    <Routes>
+      <Route index path="/" element={<LandingPage />} />
+      <Route path="/recruiters/auth" element={<Auth />} />
+      <Route path="/recruiters/companyprofile" element={<ProfilePage />} />
+      <Route path="/recruiters/dashboard" element={<RecruiterPage />} />
+    </Routes>
+  </div>
+}
 
-export default App;
+export default App
