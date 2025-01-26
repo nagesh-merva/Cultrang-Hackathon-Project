79c8ee4a1d2ce4b8065accd4e9c478aa7e8331ee
amithkumar10 <amith10mnr@gmail.com>
2025-01-26 17:19:48 +0530

    Added App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/OtherPages/LandingPage";
 import Auth from "./pages/RecruiterPage/Auth";
-import ProfilePage from "./components/Recruiter/CompanyProfile/CompanyProfile";
+
 import RecruiterPage from "./pages/RecruiterPage/RecruiterPage";
 
 const App = () => {
